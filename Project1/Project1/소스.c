71afe6fd510615a488373d68b9ee1016bde79a31
fastew <ljg7234@naver.com>
2022-09-26 00:35:54 +0900

    second commit

--- Project1/Project1/소스.c
+++ Project1/Project1/소스.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 int main() {
-	printf("hello world");
+	system("mode con cols=가로 lines=세로 | title 창 제목");
+	printf("\n\n\n\n");
+	printf("         ############         ##            ##      ##           ##\n");
+	printf("         ###        ##        ##            ##      ###          ##\n");
+	printf("         ###        ###       ##            ##      ##  #        ##\n");
+	printf("         ###        ###       ##            ##      ##   #       ##\n");
+	printf("         ##############       ##            ##      ##    #      ##\n");
+	printf("         ###        ###       ##            ##      ##     #     ##\n");
+	printf("         ###        ###       ##            ##      ##      #    ##\n");	
+	printf("         ###        ###       ################      ##       #   ##\n");
+	
 }
